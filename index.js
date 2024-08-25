7a8875cc2657d8980f137e0dfbc65a824722621c
Ibrhaim <54852400+mohdibrahim1995@users.noreply.github.com>
2024-08-25 09:25:55 +0000

    Test

---
+++ index.js
@@ -0,0 +1,9 @@
+
+ff
+
+if(actualFullName!=expectedFUllName){
+    throw Error(`${actualFullName} is not Equal to the ${expectedFUllName}`)
+}
+if(actualFullName==expectedFUllName){
+    throw Error(`${actualFullName} is Equal to ${expectedFUllName}`)
+}
