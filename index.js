4d17a82d0f17c83370a5ec5958ef2e52685efb57
Ibrhaim <mohdibrahim1995@gmail.com>
2024-08-25 14:41:32 +0530

    test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const actualFullName= getFullName('Mohd', 'Ibrahim')
 const expectedFUllName=getFullName('Mohd','Affan')
 
 if(actualFullName!=expectedFUllName){
-    throw Error(`${actualFullName} is not Equal to ${expectedFUllName}`)
+    throw Error(`${actualFullName} is not Equal to the ${expectedFUllName}`)
 }
 if(actualFullName==expectedFUllName){
     throw Error(`${actualFullName} is Equal to ${expectedFUllName}`)
